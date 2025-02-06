7f9454969352b82b1096023bd34d4680de92fe31
AvaAvarai <alice.williams.tech@gmail.com>
2025-02-06 10:14:32 -0800

    migration fixes

--- backend/backend_project/settings.py
+++ backend/backend_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'backend_project',
+    'backend_app',
     'corsheaders',
     'rest_framework',
 ]
